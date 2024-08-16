15e8bc586f29c8bd8b23b5c615b0c65d685c061b
Sebastian Carballo <sebastiancarballoes@gmail.com>
2024-08-16 13:03:35 -0300

    update carousel page

--- src/app/dashboard/carousel/page.tsx
+++ src/app/dashboard/carousel/page.tsx
@@ -1,8 +1,43 @@
+'use client'
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import { Card, CardContent } from "@/components/ui/card"
+import Autoplay from 'embla-carousel-autoplay'
+
 
 export default function Page() {
-    return (
-      <div>
-        <h1>Hello Page!</h1>
-      </div>
-    );
-  }
+  return (
+    <div className="w-full flex justify-center items-center">
+      <Carousel 
+        className="w-full max-w-xs"
+        plugins = {[Autoplay({delay :1000})]}
+        opts={{
+          dragFree: true,
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="hidden sm:flex justify-center items-center" />
+        <CarouselNext className="hidden sm:flex justify-center items-center"/>
+      </Carousel>
+    </div>
+  );
+}
